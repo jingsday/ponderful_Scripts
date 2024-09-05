a8011e45e2cf00b768dc26bbba3f99bc47b5f2cd
Jing_Y <lidiasday@gmail.com>
2024-09-05 12:57:55 +0200

    .

--- ponderful_Scripts_glm/ponderful_Scripts_glm_TN_qt.R
+++ ponderful_Scripts_glm/ponderful_Scripts_glm_TN_qt.R
@@ -12,8 +12,8 @@ library(tidyverse)
 library(ggplot2)
 library(MASS)
 
-hist(phy_che$TP)
-full_df_standardized_TP <- phy_che %>%
+hist(phy_che$TN)
+full_df_standardized_TN <- phy_che %>%
   mutate(across(c(Natural_5_qt, Aquatic_500_qt, Cropland_500_qt, 
                   Forest_500_qt, Pastures.and.open.nature_500_qt, 
                   Urban_500_qt, Animals_cont, Area, Depth, 
@@ -23,35 +23,44 @@ full_df_standardized_TP <- phy_che %>%
                 ~ scale(.)[, 1])) # Standardizing each column
 library(car)
 # Check for multicollinearity
-vif_data <- full_df_standardized_TP %>%
-  dplyr::select( TP, Natural_5_qt,Aquatic_500_qt ,Cropland_500_qt , Forest_500_qt , Pastures.and.open.nature_500_qt , Urban_500_qt, Animals_cont , Area , Depth  , Hydeoperiod_length,
+vif_data <- full_df_standardized_TN %>%
+  dplyr::select( TN, Natural_5_qt,Aquatic_500_qt ,Cropland_500_qt , Forest_500_qt , Pastures.and.open.nature_500_qt , Urban_500_qt, Animals_cont , Area , Depth  , Hydeoperiod_length,
                  ,bio4.t,bio5.t,bio12.t)
 
 # Calculate VIF values
-vif_values <- vif(lm( TP ~ ., data = vif_data))
+vif_values <- vif(lm( TN ~ ., data = vif_data))
 
 vif_values                 
 
 library(mgcv)
 ###linear model
-linear_model <- gam(TP ~ Natural_5_qt + Aquatic_500_qt + Cropland_500_qt + 
+linear_model <- gam(TN ~ Natural_5_qt + Aquatic_500_qt + Cropland_500_qt + 
                       Forest_500_qt + Pastures.and.open.nature_500_qt + 
-                      Urban_500_qt + Animals_cont.t + Area.t + Depth.t + Hydeoperiod_length.t+bio1.t+bio4.t+bio5.t+bio12.t, data = full_df_standardized_TP)
+                      Urban_500_qt + Animals_cont.t + Area.t + Depth.t + Hydeoperiod_length.t+bio1.t+bio4.t+bio5.t+bio12.t, data = full_df_standardized_TN)
 summary(linear_model)
 
+gam_model_linear <- gam(TN ~ s(Natural_5_qt) + s(Aquatic_500_qt) + 
+                          s(Cropland_500_qt) + s(Forest_500_qt) + 
+                          s(Pastures.and.open.nature_500_qt)+s(Urban_500_qt)+s(Animals_cont.t,k=7)+
+                          s(Area.t)+s(Depth.t)+s(Hydeoperiod_length.t)+s(bio1.t)+s(bio4.t)+s(bio5.t)+s(bio12.t), 
+                        data = full_df_standardized_TN)
+
+
+summary(gam_model_linear)
+
 ###poisson model
-poisson_model <- gam(TP ~ Natural_5_qt + Aquatic_500_qt + Cropland_500_qt + 
+poisson_model <- gam(TN ~ Natural_5_qt + Aquatic_500_qt + Cropland_500_qt + 
                       Forest_500_qt + Pastures.and.open.nature_500_qt + 
-                      Urban_500_qt + Animals_cont.t + Area.t + Depth.t + Hydeoperiod_length.t+bio1.t+bio4.t+bio5.t+bio12.t, data = full_df_standardized_TP,family=poisson)
+                      Urban_500_qt + Animals_cont.t + Area.t + Depth.t + Hydeoperiod_length.t+bio1.t+bio4.t+bio5.t+bio12.t, data = full_df_standardized_TN,family=poisson)
 summary(poisson_model)
 ###glm lasso
 library("glmnet")
 
-x <- as.matrix(full_df_standardized_TP[, c('Natural_5_qt', 'Aquatic_500_qt', 'Cropland_500_qt', 
+x <- as.matrix(full_df_standardized_TN[, c('Natural_5_qt', 'Aquatic_500_qt', 'Cropland_500_qt', 
                                            'Forest_500_qt', 'Pastures.and.open.nature_500_qt', 
                                            'Urban_500_qt', 'Animals_cont.t', 'Area.t', 'Depth.t', 
-                                           'Hydeoperiod_length','bio1.t','bio4.t','bio5.t','bio12.t')])
-y <- full_df_standardized_TP$TP
+                                           'Hydeoperiod_length.t','bio1.t','bio4.t','bio5.t','bio12.t')])
+y <- full_df_standardized_TN$TN
 
 hist( y)
 which(is.na(x))
@@ -61,15 +70,15 @@ x <- x[-c(212,200),]
 y<-y[-c(212,200)]
 
 
-TP_glm <- glmnet(x = x, y = y,family='poisson',alpha=1)
+TN_glm <- glmnet(x = x, y = y,family='poisson',alpha=1)
 
-print(TP_glm)
+print(TN_glm)
 cvfit<- cv.glmnet(x = x, y = y,family='poisson',alpha=1,measure='c')
 plot(cvfit)
 
 
 cvfit$lambda.min
-cfs = coef(TP_glm,s=  cvfit$lambda.min)
+cfs = coef(TN_glm,s=  cvfit$lambda.min)
 
 meaning_coefs = rownames(cfs)[cfs[,1]!= 0]
 meaning_vals = cfs[cfs[,1]!=0,]
@@ -77,38 +86,31 @@ meaning_vals
 
 
 ###gam model
-gam_model_simple <- gam(TP ~ s(Natural_5_qt) + s(Aquatic_500_qt) + 
+gam_model_simple <- gam(TN ~ s(Natural_5_qt) + s(Aquatic_500_qt) + 
                           s(Cropland_500_qt) + s(Forest_500_qt) + 
                           s(Pastures.and.open.nature_500_qt)+s(Urban_500_qt)+s(Animals_cont.t,k=7)+
                           s(Area.t)+s(Depth.t)+s(Hydeoperiod_length.t)+s(bio1.t)+s(bio4.t)+s(bio5.t)+s(bio12.t), 
-                        data = full_df_standardized_TP, family = Gamma(link = "log"))
+                        data = full_df_standardized_TN, family = Gamma(link = "log"))
 
 
 summary(gam_model_simple)
-gam_model_linear <- gam(TP ~ s(Natural_5_qt) + s(Aquatic_500_qt) + 
+
+ps_model_simple <- gam(TN ~ s(Natural_5_qt) + s(Aquatic_500_qt) + 
                           s(Cropland_500_qt) + s(Forest_500_qt) + 
                           s(Pastures.and.open.nature_500_qt)+s(Urban_500_qt)+s(Animals_cont.t,k=7)+
                           s(Area.t)+s(Depth.t)+s(Hydeoperiod_length.t)+s(bio1.t)+s(bio4.t)+s(bio5.t)+s(bio12.t), 
-                        data = full_df_standardized_TP)
-summary(gam_model_linear)
-
-
-ps_model_simple <- gam(TP ~ s(Natural_5_qt) + s(Aquatic_500_qt) + 
-                          s(Cropland_500_qt) + s(Forest_500_qt) + 
-                          s(Pastures.and.open.nature_500_qt)+s(Urban_500_qt)+s(Animals_cont.t,k=7)+
-                          s(Area.t)+s(Depth.t)+s(Hydeoperiod_length.t)+s(bio1.t)+s(bio4.t)+s(bio5.t)+s(bio12.t), 
-                        data = full_df_standardized_TP, family = poisson)
+                        data = full_df_standardized_TN, family = poisson)
 
 
 summary(ps_model_simple)
 
 
 ###gamma model 
-gamma_model <- glm(TP ~ Natural_5_qt + Aquatic_500_qt + Cropland_500_qt + 
+gamma_model <- glm(TN ~ Natural_5_qt + Aquatic_500_qt + Cropland_500_qt + 
                      Forest_500_qt + Pastures.and.open.nature_500_qt + 
-                     Urban_500_qt + Animals_cont + Area.t + Depth.t + Hydeoperiod_length.t+bio4.t+bio5.t+
+                     Urban_500_qt + Animals_cont.t + Area.t + Depth.t + Hydeoperiod_length.t+bio4.t+bio5.t+
                      bio12.t+bio1.t,
-                   data = full_df_standardized_TP, family = Gamma(link = "log"))
+                   data = full_df_standardized_TN, family = Gamma(link = "log"))
 
 summary(gamma_model)
 # Extract the null deviance and residual deviance
@@ -122,24 +124,25 @@ deviance_explained
 predictions <- predict(gamma_model, type = "response")
 plot(gamma_model)
 
-predictions <- predict(gamma_model,newdata=full_df_standardized_TP,type='response')
-plot(predictions)
-plot(full_df_standardized_TP$TP)
 
-hist(scale(full_df_standardized_TP$TP))
+predictions <- predict(gamma_model,newdata=full_df_standardized_TN,type='response')
+plot(predictions)
+hist(full_df_standardized_TN$TN)
+
+hist(scale(full_df_standardized_TN$TN))
 ###Mixed model
 library(lme4)
 
 # Create a factor variable indicating the segment
-full_df_standardized_TP$segment <- ifelse(full_df_standardized_TP$TP <= 1, "low", "high")
+full_df_standardized_TN$segment <- ifelse(full_df_standardized_TN$TN <= 1, "low", "high")
 
 # Fit a mixed model with different intercepts for each segment
-mixed_model <- lmer(TP ~ Natural_5_qt + Aquatic_500_qt + Cropland_500_qt + 
+mixed_model <- lmer(TN ~ Natural_5_qt + Aquatic_500_qt + Cropland_500_qt + 
                       Forest_500_qt + Pastures.and.open.nature_500_qt + 
-                      Urban_500_qt + Animals_cont.t + Area.t + Depth.t + Hydeoperiod_length.t+bio1.t+bio4.t+bio5.t+bio12.t + (1 | segment), data = full_df_standardized_TP)
+                      Urban_500_qt + Animals_cont.t + Area.t + Depth.t + Hydeoperiod_length.t+bio1.t+bio4.t+bio5.t+bio12.t + (1 | segment), data = full_df_standardized_TN)
 summary(mixed_model)
 
-null_model <- lmer(TP ~1 + (1|segment),data =full_df_standardized_TP)
+null_model <- lmer(TN ~1 + (1|segment),data =full_df_standardized_TN)
 # Calculate log-likelihood for both models
 null_logLik <- logLik(null_model)
 fitted_logLik <- logLik(mixed_model)
@@ -171,16 +174,16 @@ cat("Fitted Model Deviance:", fitted_deviance, "\n")
 
 ##gamma model selected
 
-tp_gamma_model <- glm(TP ~ Natural_5_qt + Aquatic_500_qt + Cropland_500_qt + 
+TN_gamma_model <- glm(TN ~ Natural_5_qt + Aquatic_500_qt + Cropland_500_qt + 
                         Forest_500_qt + Pastures.and.open.nature_500_qt + 
                         Urban_500_qt + Animals_cont.t + Area.t + Depth.t + Hydeoperiod_length.t+bio4.t+bio5.t+
                         bio1.t+ bio12.t,
-                      data = full_df_standardized_TP, family = Gamma(link = "log"))
+                      data = full_df_standardized_TN, family = Gamma(link = "log"))
 
-summary(tp_gamma_model)
+summary(TN_gamma_model)
 
-null_dev <- tp_gamma_model$null.deviance
-res_dev <- tp_gamma_model$deviance
+null_dev <- TN_gamma_model$null.deviance
+res_dev <- TN_gamma_model$deviance
 
 # Calculate deviance explained
 deviance_explained <- 1 - (res_dev / null_dev)
@@ -188,8 +191,8 @@ deviance_explained
 
 #selected
 
-gamma_model_selected <- glm(TP ~ Natural_5_qt + Animals_cont + Area + Depth + bio5.t+bio12.t,
-                   data = full_df_standardized_TP, family = Gamma(link = "log"))
+gamma_model_selected <- glm(TN ~ Natural_5_qt + Animals_cont + Area + Depth + bio5.t+bio12.t,
+                   data = full_df_standardized_TN, family = Gamma(link = "log"))
 
 plot(gamma_model_selected)
 
@@ -222,27 +225,27 @@ summary(tn_gamma_model)
 predictions <- predict(gamma_model, type = "response")
 plot(tn_gamma_model)
 
-predictions <- predict(gamma_model,newdata=full_df_standardized_TP,type='response')
+predictions <- predict(gamma_model,newdata=full_df_standardized_TN,type='response')
 plot(predictions)
-plot(full_df_standardized_TP$TP)
+plot(full_df_standardized_TN$TN)
 
 
 
 # Fit the GAM
-gam_model <- gam(TP ~ s(Natural_5_qt) + s(Aquatic_500_qt) + s(Cropland_500_qt) + 
+gam_model <- gam(TN ~ s(Natural_5_qt) + s(Aquatic_500_qt) + s(Cropland_500_qt) + 
                    s(Forest_500_qt) + s(Pastures.and.open.nature_500_qt) + 
                    s(Urban_500_qt) + s(Animals_cont) + s(Area) + s(Depth) + 
                    s(Hydeoperiod_length) +  s(bio5.t) + s(bio12.t) , 
-                 data = full_df_standardized_TP, family = Gamma(link = "log"))
+                 data = full_df_standardized_TN, family = Gamma(link = "log"))
 
-gam_model <- gam(TP ~ s(Natural_5_qt, k = 5) + s(Aquatic_500_qt, k = 5) + 
+gam_model <- gam(TN ~ s(Natural_5_qt, k = 5) + s(Aquatic_500_qt, k = 5) + 
                    s(Cropland_500_qt, k = 5) + s(Forest_500_qt, k = 5) + 
                    s(Pastures.and.open.nature_500_qt, k = 5) + s(Urban_500_qt, k = 5) + 
                    s(Animals_cont, k = 5) + s(Area, k = 5) + s(Depth, k = 5) + 
                    s(Hydeoperiod_length, k = 5) + s(bio4.t, k = 5) + s(bio5.t, k = 5) + 
                    s(bio6.t, k = 5) + s(bio7.t, k = 5) + s(bio12.t, k = 5) + 
                    s(bio15.t, k = 5), 
-                 data = full_df_standardized_TP, family = Gamma(link = "log"))
+                 data = full_df_standardized_TN, family = Gamma(link = "log"))
 
 
 # Model summary
@@ -268,35 +271,35 @@ qqline(residuals(gam_model), col = "red")
 plot(gam_model, pages = 1)
 
 # Predictions
-predictions <- predict(gam_model, newdata = full_df_standardized_TP)
+predictions <- predict(gam_model, newdata = full_df_standardized_TN)
 
 
-gam_model_simple <- gam(TP ~ s(Natural_5_qt) + s(Aquatic_500_qt) + 
+gam_model_simple <- gam(TN ~ s(Natural_5_qt) + s(Aquatic_500_qt) + 
                           s(Cropland_500_qt) + s(Forest_500_qt) + 
                           s(Pastures.and.open.nature_500_qt), 
-                        data = full_df_standardized_TP, family = Gamma(link = "log"))
+                        data = full_df_standardized_TN, family = Gamma(link = "log"))
 
 summary(gam_model_simple)
 
 
-vif(lm(TP ~ Natural_5_qt + Aquatic_500_qt + Cropland_500_qt + 
+vif(lm(TN ~ Natural_5_qt + Aquatic_500_qt + Cropland_500_qt + 
          Forest_500_qt + Pastures.and.open.nature_500_qt + 
          Urban_500_qt + Animals_cont + Area + Depth + Hydeoperiod_length, 
-       data = full_df_standardized_TP))
+       data = full_df_standardized_TN))
 
 
 #inverse Gaussian Regression
-inv_gaussian_model <- glm(TP ~ Natural_5_qt + Aquatic_500_qt + Cropland_500_qt + 
+inv_gaussian_model <- glm(TN ~ Natural_5_qt + Aquatic_500_qt + Cropland_500_qt + 
                             Forest_500_qt + Pastures.and.open.nature_500_qt + 
                             Urban_500_qt + Animals_cont + Area + Depth + Hydeoperiod_length, 
-                          data = full_df_standardized_TP, 
+                          data = full_df_standardized_TN, 
                           family = inverse.gaussian(link = "log"),
                           control = glm.control(maxit = 100))
 par(mfrow = c(2, 2))
 plot(inv_gaussian_model)
 
 
-full_model_TP_arable <- lmer( TP ~  Natural_5_qt+Aquatic_500_qt+Cropland_500_qt+ Forest_500_qt+ Pastures.and.open.nature_500_qt+ Urban_500_qt+ Animals_cont+ Area + Depth+ Hydeoperiod_length +
+full_model_TN_arable <- lmer( TN ~  Natural_5_qt+Aquatic_500_qt+Cropland_500_qt+ Forest_500_qt+ Pastures.and.open.nature_500_qt+ Urban_500_qt+ Animals_cont+ Area + Depth+ Hydeoperiod_length +
                                 (1 | Country) + 
                                 (0 + Natural_5_qt | Country) + 
                                 (0 + Aquatic_500_qt | Country) + 
@@ -306,10 +309,10 @@ full_model_TP_arable <- lmer( TP ~  Natural_5_qt+Aquatic_500_qt+Cropland_500_qt+
                                 (0 + Urban_500_qt | Country)+
                                 (0 + Animals_cont | Country)+
                                 (0 + Hydeoperiod_length| Country),
-                              data = full_df_standardized_TP, na.action = na.omit)
+                              data = full_df_standardized_TN, na.action = na.omit)
 
 
-summ(full_model_TP_arable)
+summ(full_model_TN_arable)
 
 null_deviance <- inv_gaussian_model$null.deviance
 residual_deviance <- inv_gaussian_model$deviance
@@ -325,18 +328,18 @@ abline(h = 0, col = "red")
 
 
 #AIC 
-full_model_TP_ml <- lme(fixed=TP ~ Natural_5_qt+Aquatic_500_qt+Cropland_500_qt+ Forest_500_qt+ Pastures.and.open.nature_500_qt+ Urban_500_qt+ Animals_cont+ Area + Depth+ Hydeoperiod_length+T1+P1,
+full_model_TN_ml <- lme(fixed=TN ~ Natural_5_qt+Aquatic_500_qt+Cropland_500_qt+ Forest_500_qt+ Pastures.and.open.nature_500_qt+ Urban_500_qt+ Animals_cont+ Area + Depth+ Hydeoperiod_length+T1+P1,
                         random = list(~1 | Country, ~0 + Natural_5_qt | Country, ~0 + Aquatic_500_qt | Country, ~0 + Cropland_500_qt | Country, 
                                       ~0 + Pastures.and.open.nature_500_qt | Country, ~0 + Forest_500_qt | Country, ~0 + Urban_500_qt | Country, ~0 + Animals_cont | Country, ~0 + Hydeoperiod_length | Country),
-                        data = full_df_standardized_TP, method = 'ML', na.action = na.omit,control = lmeControl(opt = "optim")
+                        data = full_df_standardized_TN, method = 'ML', na.action = na.omit,control = lmeControl(opt = "optim")
 )
 
-stepAIC(full_model_TP_ml,control=lmeControl(opt = "optim"))
+stepAIC(full_model_TN_ml,control=lmeControl(opt = "optim"))
 
-aic_model_TP<- lme(fixed= TP ~ Forest_500_qt + Pastures.and.open.nature_500_qt + Animals_cont +      Area + Depth ,
+aic_model_TN<- lme(fixed= TN ~ Forest_500_qt + Pastures.and.open.nature_500_qt + Animals_cont +      Area + Depth ,
                    random = list( ~1|Country,~ 0+Open_nature_100|Country, ~0+Forest_100|Country,~0+Animals_cont|Country,  ~0+Area|Country, ~0+Depth|Country),
-                   data = full_df_standardized_TP, method='ML',na.action = na.omit)
+                   data = full_df_standardized_TN, method='ML',na.action = na.omit)
 
-summary(aic_model_TP)
+summary(aic_model_TN)
 
 
