d869d7cf0c0b73dbaa26b47509afd1121f8af865
Jing_Y <lidiasday@gmail.com>
2024-09-13 18:43:32 +0200

    .

--- ponderful_Scripts_glm/ponderful_Scripts_report.R
+++ ponderful_Scripts_glm/ponderful_Scripts_report.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(mgcv)
+library(ggplot2)
 
 #Loading input
 phy_che <- read.csv('/Users/lidiayung/PhD_project/project_PONDERFUL/ponderful_OUTPUT/Phy_Che_qt.csv')
@@ -26,12 +27,14 @@ full_df_standardized_TN <- phy_che %>%
                 ~ scale(.)[, 1])) # Standardizing each column
 
 
+
+
 model_TN_df <- full_df_standardized_TN[,c('TN','Natural_5_qt', 'Aquatic_500_qt', 'Cropland_500_qt', 
                          'Forest_500_qt', 'Pastures.and.open.nature_500_qt', 
                          'Urban_500_qt', 'Animals_cont.t', 'Area.t', 'Depth.t', 
                          'bio1.t','bio4.t','bio5.t',
                          'bio6.t','bio7.t',
-                         'bio12.t','bio15.t','bio17.t','Country','bioregion')]
+                         'bio12.t','bio15.t','bio17.t','Country','bioregion','bio7')]
 
 
 
@@ -58,20 +61,34 @@ model_TP_df <- full_df_standardized_TP[,c('TP','Natural_5_qt', 'Aquatic_500_qt',
 ###GAM gamma model 
 TP_gam_model_gamma <- gam(TP ~ s(Natural_5_qt) + s(Aquatic_500_qt) + 
                 s(Cropland_500_qt) + s(Forest_500_qt) + 
-                s(Pastures.and.open.nature_500_qt)+s(Urban_500_qt)+s(Animals_cont.t,k=7)+
-                s(Area.t)+s(Depth.t)+s(bio1.t)+s(bio4.t)+s(bio5.t)+s(bio12.t), 
+                s(Pastures.and.open.nature_500_qt)+s(Urban_500_qt)+s(Animals_cont.t,k=5)+
+                s(Area.t)+s(Depth.t)+bio1.t+s(bio4.t)+s(bio5.t)+s(bio12.t), 
               data = model_TP_df, family = Gamma(link = "log"))
 
 summary(TP_gam_model_gamma)
+gam.check(TP_gam_model_gamma)
+plot(TP_gam_model_gamma,page=2,shade=TRUE,rug=TRUE)
+TP_gam_model_gamma <- gam(TP ~  s(Aquatic_500_qt) + 
+                            Forest_500_qt + 
+                            s(Pastures.and.open.nature_500_qt)+s(Animals_cont.t,k=7)+
+                            s(Area.t)+Depth.t+s(bio1.t)+s(bio4.t)+s(bio12.t), 
+                          data = model_TP_df, family = Gamma(link = "log"))
+
+summary(TP_gam_model_gamma)
+gam.check(TP_gam_model_gamma)
+plot(TP_gam_model_gamma,page=1,shade=TRUE,rug=TRUE)
+
 ###selected best gam model
-TP_gam_model_gamma_selected <- gam(TP ~ s(Aquatic_500_qt) + 
-                                     s(Forest_500_qt) + 
-                                     s(Pastures.and.open.nature_500_qt)+s(Animals_cont.t,k=7)+
-                                     s(Area.t)+s(Depth.t)+s(bio1.t)+s(bio4.t)+s(bio12.t), 
+TP_gam_model_gamma_selected <- gam(TP ~  s(Aquatic_500_qt,k=2) + 
+                                     Forest_500_qt + 
+                                     s(Pastures.and.open.nature_500_qt,k=3)+s(Animals_cont.t,k=7)+
+                                     s(Area.t,k=2)+Depth.t+s(bio1.t,k=9)+s(bio4.t,k=3)+s(bio12.t,k=4), 
                                    data = model_TP_df, family = Gamma(link = "log"))
 
 summary(TP_gam_model_gamma_selected)
-
+AIC(TP_gam_model_gamma)
+plot(TP_gam_model_gamma_selected,page=1,shade=TRUE,rug=TRUE)
+gam.check(TP_gam_model_gamma_selected)
 par(mfrow = c(1, 2), cex = 1.1)
 
 plot(TP_gam_model_gamma_selected$fitted.values, residuals(TP_gam_model_gamma_selected), 
@@ -86,7 +103,7 @@ predictions <- predict(TP_gam_model_gamma_selected, type = "response")
 
 summary(predictions)
 dev.off()
-plot(TP_gam_model_gamma_selected,page=1)
+plot(predictions)
 
 ###linear model 
 library(MASS)
@@ -102,18 +119,22 @@ stepAIC(TP_linear_model, direction = "both")
 
 
 ###stepwise selection 
-TP_linear_model_selected <- glm(formula = TP ~ Natural_5_qt + Forest_500_qt + Pastures.and.open.nature_500_qt + 
-               Animals_cont.t + Depth.t + bio5.t + bio12.t, data = model_TP_df)
+TP_linear_model_selected <- glm(formula =  TP ~ Natural_5_qt + Forest_500_qt + Pastures.and.open.nature_500_qt + 
+                                  Animals_cont.t + Depth.t + bio5.t + bio12.t, data = model_TP_df)
 
 
 summary(TP_linear_model_selected)
-AIC(TP_glm_reg_stepwise)
+AIC(TP_linear_model_selected)
 
 ###Disgnostics (Deviance explained= 1-(residual/null))
  1 - (45.76 / 59.99)
- plot(TP_linear_model_selected)
+par(mfrow = c(2, 2))
+
+
+plot(TP_linear_model_selected)
  
- ## TN
+ 
+## TN
  ###GAMM gamma model 
 TN_gam_model <- gam(TN ~ s(Natural_5_qt) + s(Aquatic_500_qt) + 
                           s(Cropland_500_qt) + s(Forest_500_qt) + 
@@ -203,24 +224,34 @@ cor(df)
 
 ### Bio-climatic regions TP model
 ####TP Medi
+input<- 'Mediterranean'
 df <-model_TP_df[model_TP_df$bioregion == input,][,c('TP','Natural_5_qt',  'Cropland_500_qt', 'Urban_500_qt',  'Depth.t', 
                                                      'Pastures.and.open.nature_500_qt', 'Aquatic_500_qt','Animals_cont.t', 'bio1.t','bio4.t','bio5.t','bio12.t')]
 hist(df$TP)
 
-TP_gam_medi <- gam(TP ~ s(Natural_5_qt,k=9) + s(Aquatic_500_qt,k=6) + 
+TP_gam_medi <- gam(TP ~ s(Natural_5_qt) + s(Aquatic_500_qt) + 
                      s(Cropland_500_qt) +
                      s(Pastures.and.open.nature_500_qt) + s(Urban_500_qt) +
-                     s(Animals_cont.t, k = 6) + s(Depth.t)+ s(bio1.t),  
+                     s(Animals_cont.t) + s(Depth.t)+ s(bio1.t),  
                    data = df, family = Gamma(link = "log"))
+
 length(unique(df$Aquatic_500_qt))
 summary(TP_gam_medi)
+gam.check(TP_gam_medi)
 
-TP_gam_medi_selected<-gam(TP ~ s(Natural_5_qt) + s(Aquatic_500_qt) + 
-                           s(Cropland_500_qt) +
-                           s(Pastures.and.open.nature_500_qt) + s(Depth.t),  
+TP_gam_medi_selected<-gam(TP ~ Natural_5_qt + s(Cropland_500_qt,k=3) + Urban_500_qt +Animals_cont.t+bio1.t,
                          data = df, family = Gamma(link = "log"))
 summary(TP_gam_medi_selected)
+plot(TP_gam_medi_selected,page=1,rug=TRUE,shade=TRUE)
+AIC(TP_gam_medi_selected)
 
+###Diagnostics 
+
+par(mfrow = c(2, 2))
+gam.check(TP_gam_medi_selected)
+
+predictions <- predict(TP_gam_medi_selected, type = "response")
+summary(predictions)
 
 ####TP Continental
 input <-'Continental'
@@ -228,25 +259,54 @@ df <-model_TP_df[model_TP_df$bioregion == input,][,c('TP','Natural_5_qt',  'Crop
                                                      'Forest_500_qt','bio7.t')]
 hist(df$TP)
 
-TP_gam_ctn <- gam(TP ~ s(Natural_5_qt) + s(Forest_500_qt)+s(Cropland_500_qt) +
-                    s(Urban_500_qt) + s(Depth.t)+ s(bio7.t,k=5),  
+TP_gam_ctn <- gam(TP ~ Natural_5_qt + s(Forest_500_qt,k=3)+s(Cropland_500_qt,k=2) +
+                    s(Urban_500_qt,k=) + Depth.t+ bio7.t,  
                    data = df, family = Gamma(link = "log"))
 
 summary(TP_gam_ctn)
 plot(TP_gam_ctn)
+
+TP_gam_ctn <- gam(TP ~ s(Forest_500_qt,k=5)+ Cropland_500_qt +s(Urban_500_qt,k=3) +  bio7.t,
+                  data = df, family = Gamma(link = "log"))
+
+TP_gam_ctn_selected <- gam(TP ~ s(Forest_500_qt) + Cropland_500_qt + s(Urban_500_qt, k = 2) + s(Depth.t,k=6) + bio7.t,  
+                           data = df, family = Gamma(link = "log"))
+
+summary(TP_gam_ctn_selected)
+plot(TP_gam_ctn_selected,page=1,shade=TRUE,rug=TRUE)
+AIC(TP_gam_ctn)
+
+par(mfrow= c(2, 2), cex = 1.1)
+gam.check(TP_gam_ctn_selected)
+
 
 ####TP Temperate
 input <-'Temperate'
 df <-model_TP_df[model_TP_df$bioregion == input,][,c('TP','Natural_5_qt',  'Cropland_500_qt', 'Urban_500_qt',  'Depth.t', 
                                                      'Pastures.and.open.nature_500_qt', 'Aquatic_500_qt','Animals_cont.t', 'bio1.t','bio5.t')]
 hist(df$TP)
-TP_gam_temp <- gam(TP ~ s(Natural_5_qt) + s(Aquatic_500_qt) + 
-                     s(Cropland_500_qt) +
-                     s(Pastures.and.open.nature_500_qt) + s(Urban_500_qt) +
-                     s(Animals_cont.t, k = 5) + s(Depth.t)+ s(bio5.t)+s(bio1.t),  
+TP_gam_temp <- gam(TP ~ Natural_5_qt + Aquatic_500_qt + 
+                     Cropland_500_qt +
+                     Pastures.and.open.nature_500_qt + Urban_500_qt +
+                     s(Animals_cont.t,k=4) + Depth.t+s(bio1.t,k=5),  
                    data = df, family = Gamma(link = "log"))
+gam.check(TP_gam_temp)
 summary(TP_gam_temp)
 
+gam.check(TP_gam_temp)
+TP_gam_temp <- gam(TP ~  s(Cropland_500_qt,k=5) +
+                    s(Pastures.and.open.nature_500_qt) + 
+                    s(Animals_cont.t,k=4) + s(Depth.t)+ s(bio1.t),  
+                  data = df, family = Gamma(link = "log"))
+summary(TP_gam_temp)
+
+TP_gam_temp_selected <- gam(TP ~ s(Cropland_500_qt, k = 5) + s(Pastures.and.open.nature_500_qt) + 
+                              s(Animals_cont.t, k = 4) + s(Depth.t) + s(bio1.t),  
+                           data = df, family = Gamma(link = "log"))
+
+summary(TP_gam_temp_selected)
+plot(TP_gam_temp_selected,page=1,shade=TRUE,rug=TRUE)
+AIC(TP_gam_temp_selected)
 ####TP Subtropical
 input <-'Subtropical'
 df <-model_TP_df[model_TP_df$bioregion == input,][,c('TP','Natural_5_qt',  'Cropland_500_qt', 'Animals_cont.t',
@@ -268,8 +328,9 @@ summary(TP_gam_subt)
 input <- 'Mediterranean'
 #medi
 df <-model_TN_df[model_TN_df$bioregion == input,][,c('TN','Natural_5_qt',  'Cropland_500_qt', 'Urban_500_qt',  'Depth.t', 
-                                                     'Pastures.and.open.nature_500_qt', 'Aquatic_500_qt','Animals_cont.t', 'bio1.t')]
+                                                     'Pastures.and.open.nature_500_qt', 'Aquatic_500_qt','Animals_cont.t', 'bio1.t','Country')]
 hist(df$TN)
+
 
 TN_gam_medi <- gam(TN ~ s(Natural_5_qt,k=9) + s(Aquatic_500_qt,k=6) + 
                      s(Cropland_500_qt) +
@@ -282,18 +343,37 @@ summary(TN_gam_medi)
 input<- 'Continental'
 
 df <-model_TN_df[model_TN_df$bioregion == input,][,c('TN','Natural_5_qt',  'Cropland_500_qt', 'Urban_500_qt',  'Depth.t', 
-                                                     'Forest_500_qt','bio7.t')]  
-hist(df$TN)
-TN_gam_ctn <-gam(TN ~ s(Natural_5_qt,k=9) + s(Forest_500_qt)+
+                                                     'Forest_500_qt','bio7.t','bio7')] 
+
+TN_gam_ctn <-gam(TN ~ s(Natural_5_qt,k=3) + s(Forest_500_qt,k=7)+
                    s(Cropland_500_qt) + s(Urban_500_qt) +s(Depth.t)+ s(bio7.t),  
                  data = df, family = Gamma(link = "log"))
 summary(TN_gam_ctn)
+plot(TN_gam_ctn,pages=1,shade=TRUE,rug=TRUE)
+
+
+TN_gam_ctn_selected <-gam(TN ~ s(Forest_500_qt,k=4)+
+                   Cropland_500_qt + Urban_500_qt +Depth.t+ s(bio7.t),  
+                 data = df, family = Gamma(link = "log"))
+summary(TN_gam_ctn_selected)
+gam.check(TN_gam_ctn_selected)
+coef(TN_gam_ctn_selected)
+TN_gam_ctn_selected$smooth
+plot(TN_gam_ctn_selected,pages=1,shade=TRUE,rug=TRUE)
+
+
+plot(TN_gam_ctn_selected,pages=1,shade=TRUE,rug=TRUE)
 
 #Temperate
 input<- 'Temperate'
 df <-model_TN_df[model_TN_df$bioregion == input,][,c('TN','Natural_5_qt',  'Cropland_500_qt', 
                                                      'Urban_500_qt',  'Depth.t', 
-                                                     'Pastures.and.open.nature_500_qt', 'Aquatic_500_qt','Animals_cont.t', 'bio5.t')]
+                                                     'Pastures.and.open.nature_500_qt', 'Aquatic_500_qt','Animals_cont.t', 'bio5.t','Country')]
+table(df$Country)
+df_country <-model_TN_df[model_TN_df$Country == 'UK',]
+hist(df_country$TN)
+
+
 hist(df$TN)
 TN_gam_temp <-gam(TN ~ s(Natural_5_qt) + s(Aquatic_500_qt)+s(Animals_cont.t,k=5)+s(Pastures.and.open.nature_500_qt)+
                    s(Cropland_500_qt) + s(Urban_500_qt) +s(Depth.t)+ s(bio5.t),  
